8d68f71cc0f12c86dd5e12a899e72b6c007a8037
Huda Ridwan <kerupuk.lovers@gmail.com>
2017-10-06 16:52:39 +0700

    Add all files project html, css, js

---
+++ js/index.js
@@ -0,0 +1,27 @@
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+// Click on a close button to hide the current list item
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+function newElement() {
+    var node = document.createElement("LI");
+    var textnode = document.createTextNode("myInput");
+    node.appendChild(textnode);
+    document.getElementById("myList").appendChild(node);
+}
+
